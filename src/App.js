d0ddf384df13126e910f02b9921daee9f533888e
apautvein <apautvein@gmail.com>
2023-04-12 17:23:29 -0500

    add background and remove unused items. WIP landing page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline pt-6 text-red-600">Hello World</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="text-5xl font-sans font-normal pt-6 text-black-50">Alexis Crandall</h1>
       </header>
+      <div className="bg-contain min-h-screen" style={{backgroundImage: "url(/background.jpg)"}}>
+        <p>Visit Me On:</p>
+          <a
+            className="App-link"
+            href="https://github.com/apautvein"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github 
+          </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+      </div>
     </div>
   );
 }
