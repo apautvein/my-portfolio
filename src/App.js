61ea6af73f60b9aefcb6216c5a26f533daa085c0
apautvein <apautvein@gmail.com>
2023-05-03 16:28:02 -0500

    add social media links.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
 import Navigation from './Navigation';
+import SocialMedia from './SocialMedia';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <div className="bg-contain min-h-screen" style={{ backgroundImage: "url(/background.jpg)" }}>
-        <p>Visit Me On:</p>
-        <a
-          className="App-link"
-          href="https://github.com/apautvein"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
+       <div>Hello my name is Alexis Crandall</div>
+       <div>Fullstack Software Engineer</div>
+        <SocialMedia/>
       </div>
     </div>
   );
---
+++ src/SocialMedia.js
@@ -0,0 +1,14 @@
+function SocialMedia() {
+    return (
+        <div className='bg-white space-y-6 fixed p-5 rounded mt-40'>
+            <div>
+                <a href="https://www.linkedin.com/in/alexis-crandall-409a76158"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+            </div>
+            <div>
+                <a href="https://github.com/apautvein"><i class="fa-brands fa-github fa-2xl"></i></a>
+            </div>
+        </div>
+    );
+}
+
+export default SocialMedia;
